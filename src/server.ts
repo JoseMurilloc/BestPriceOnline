1bee4266e64de15f1bf6be5d9439e73e5699c619
Jose Murillo <jooseemurillo@gmail.com>
2020-06-20 18:10:40 -0300

    Add config typeorm create conn

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import routes from './app/routes'
 
+import './app/database'
+
 const app = express()
 
 app.use(routes)
 
-app.listen(3333)
+app.listen(3333, () => console.log('Server runing port http://localhost:3333 🚀'))
