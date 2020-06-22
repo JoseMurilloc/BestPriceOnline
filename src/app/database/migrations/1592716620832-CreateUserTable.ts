c323ac29403059a2b9b995c566c2d7a0bd925474
Jose Murillo <jooseemurillo@gmail.com>
2020-06-21 21:48:28 -0300

    Migration: create table User

---
+++ src/app/database/migrations/1592716620832-CreateUserTable.ts
@@ -0,0 +1,38 @@
+// eslint-disable-next-line no-unused-vars
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateUserTable1592716620832 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
