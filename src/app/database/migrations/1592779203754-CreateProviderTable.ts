1edd9687786809059f7bc8c0477f5af048fce2b3
Jose Murillo <jooseemurillo@gmail.com>
2020-06-21 22:01:12 -0300

    Migration: create table providers

---
+++ src/app/database/migrations/1592779203754-CreateProviderTable.ts
@@ -0,0 +1,59 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm'
+
+export class CreateProviderTable1592779203754 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'providers',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar'
+          },
+          {
+            name: 'amount_offer',
+            type: 'integer',
+            default: 0
+          },
+          {
+            name: 'user_id',
+            type: 'integer'
+          }
+        ]
+      })
+    )
+
+    await queryRunner.createForeignKey('providers', new TableForeignKey(
+      {
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE'
+      }
+    ))
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('users')
+    const foreignKey = table.foreignKeys.find(fk => fk.columnNames.indexOf('user_id') !== -1)
+
+    await queryRunner.dropForeignKey('users', foreignKey)
+    await queryRunner.dropTable('providers')
+  }
+}
