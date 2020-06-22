6f857165e40c8cfa0a2071b86221d244466afd1b
Jose Murillo <jooseemurillo@gmail.com>
2020-06-21 22:02:04 -0300

    Migration: create table categories

---
+++ src/app/database/migrations/1592784786441-CreateCategoryTable.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey, TableColumn } from 'typeorm'
+
+export class CreateCategoryTable1592784786441 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+            isNullable: false
+          }
+        ]
+      })
+    )
+
+    await queryRunner.addColumn('products', new TableColumn({
+      name: 'category_id',
+      type: 'integer',
+      isNullable: false
+    }))
+
+    await queryRunner.createForeignKey('products', new TableForeignKey(
+      {
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'CASCADE'
+      }
+    ))
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('categories')
+    const foreignKey = table.foreignKeys.find(fk => fk.columnNames.indexOf('category_id') !== -1)
+
+    await queryRunner.dropForeignKey('categories', foreignKey)
+
+    await queryRunner.dropTable('categories')
+  }
+}
