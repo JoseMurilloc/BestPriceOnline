9d5ed70294541ee8ca16b766800b8099041f766f
Jose Murillo <jooseemurillo@gmail.com>
2020-06-22 21:05:26 -0300

    Auth Middlware users

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,36 @@
+
+import { verify } from 'jsonwebtoken'
+import authConfig from '../config/auth'
+import { Request, Response, NextFunction } from 'express'
+
+interface TokenPayload {
+  provider: boolean;
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token inválido' })
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const decoded = verify(token, authConfig.secret)
+    // decoded basicamente é os dados do payload do Token
+    const { provider, sub } = decoded as TokenPayload
+
+    req.user = {
+      id: sub,
+      provider
+    }
+
+    return next()
+  } catch (err) {
+    return res.status(401).json({ error: 'Token Invalid' })
+  }
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,9 +3,16 @@ import { Router } from 'express'
 import UserController from './controllers/UserController'
 import SessionController from './controllers/SessionController'
 
+// import AuthMiddleware from './middlewares/auth'
+
 const routes = Router()
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+
+// routes.get('/test', AuthMiddleware, (req, res) => {
+//   console.log(req.user)
+//   return res.json({ message: 'Aprovado' })
+// })
 
 export default routes
